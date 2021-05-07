266e4978b4105d21f5747eae5dd897367b404423
Robin Hillyard <r.hillyard@northeastern.edu>
2021-05-06 21:24:00 -0400

    Added TicTacToeInt but haven't tested Evaluate with it.
    TicTacToeOperations: various new methods;
    package: new implicit class Outcome;
    TicTacToeInt: fleshed out methods;

--- src/main/java/com/phasmidsoftware/decisiontree/examples/tictactoe/TicTacToeIntOperations.java
+++ src/main/java/com/phasmidsoftware/decisiontree/examples/tictactoe/TicTacToeIntOperations.java
@@ -1,6 +1,45 @@
 package com.phasmidsoftware.decisiontree.examples.tictactoe;
 
 public class TicTacToeIntOperations {
+
+    /**
+     * Parse and array of ints, each one either 0 (open), 1 (X), or 2 (0).
+     *
+     * @param a an array of 9 ints.
+     * @return the encoding of the board.
+     */
+    public static int parse(int[] a) {
+        int result = 0;
+        for (int i = 0; i < 9; i++) result = result << 2 | a[i];
+        return result << 14;
+    }
+
+    public static int[][] asArray(int x) {
+        // CONSIDER is there a more efficient way to count the bits?
+        int[][] result = new int[3][3];
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+                int y = x & 0xC0000000;
+                result[i][j] = y >> 30;
+                x = x << 2;
+            }
+        return result;
+    }
+
+    public static int[] open(int x) {
+        // CONSIDER is there a more efficient way to count the bits?
+        int[] empty = new int[9];
+        int count = 0;
+        for (int i = 0; i < 9; i++) {
+            int y = x & 0xC0000000;
+            if (y == 0xC0000000 || y == 0x00000000) empty[count++] = i;
+            x = x << 2;
+        }
+        int[] result = new int[count];
+        System.arraycopy(empty, 0, result, 0, count);
+        return result;
+    }
+
     public static String render(int x) {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 3; i++) {
@@ -19,7 +58,7 @@ public class TicTacToeIntOperations {
         return x | bits;
     }
 
-    public static int transpose(int x) {
+    public static int transposeBoard(int x) {
         long y = (long) x & 0x00000000FFFFFFFFL;
         long z = y | flip(y, 0, 0) | flip(y, 1, 2) | flip(y, 2, 4) | flip(y, 3, -2) | flip(y, 4, 0) | flip(y, 5, 2) | flip(y, 6, -4) | flip(y, 7, -2) | flip(y, 8, 0);
         return (int) (z >> 32);
--- src/test/scala/com/phasmidsoftware/decisiontree/examples/tictactoe/TicTacToeIntSpec.scala
+++ src/test/scala/com/phasmidsoftware/decisiontree/examples/tictactoe/TicTacToeIntSpec.scala
@@ -1,14 +1,20 @@
 package com.phasmidsoftware.decisiontree.examples.tictactoe
 
-import com.phasmidsoftware.decisiontree.examples.tictactoe.TicTacToe.stride
+import com.phasmidsoftware.decisiontree.examples.tictactoe.TicTacToeInt.stride
 import com.phasmidsoftware.decisiontree.examples.tictactoe.TicTacToeIntOperations._
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 
 class TicTacToeIntSpec extends AnyFlatSpec with should.Matchers {
 
     behavior of "TicTacToeIntOperations"
+    it should "open" in {
+        TicTacToeIntOperations.open(0) shouldBe Array(0, 1, 2, 3, 4, 5, 6, 7, 8)
+        TicTacToeIntOperations.open(0x40000000) shouldBe Array(1, 2, 3, 4, 5, 6, 7, 8)
+        TicTacToeIntOperations.open(0x0C000000) shouldBe Array(0, 1, 2, 3, 4, 5, 6, 7, 8)
+        TicTacToeIntOperations.open(0x40400000) shouldBe Array(1, 2, 3, 5, 6, 7, 8)
+    }
     it should "render" in {
         render(0x40000000) shouldBe "X..\n...\n...\n"
     }
@@ -20,30 +26,34 @@ class TicTacToeIntSpec extends AnyFlatSpec with should.Matchers {
         board2 shouldBe 0x40080000
     }
     it should "transpose to Int" in {
-        transpose(0xC0000000) shouldBe 0xC0000000
-        transpose(0x30000000) shouldBe 0x03000000
-        transpose(0x0C000000) shouldBe 0x000C0000
-        transpose(0x03000000) shouldBe 0x30000000
-        transpose(0x00C00000) shouldBe 0xc00000
-        transpose(0x00300000) shouldBe 0x30000
-        transpose(0x000C0000) shouldBe 0xc000000
-        transpose(0x00030000) shouldBe 0x300000
-        transpose(0x0000C000) shouldBe 0xc000
+        transposeBoard(0xC0000000) shouldBe 0xC0000000
+        transposeBoard(0x30000000) shouldBe 0x03000000
+        transposeBoard(0x0C000000) shouldBe 0x000C0000
+        transposeBoard(0x03000000) shouldBe 0x30000000
+        transposeBoard(0x00C00000) shouldBe 0xc00000
+        transposeBoard(0x00300000) shouldBe 0x30000
+        transposeBoard(0x000C0000) shouldBe 0xc000000
+        transposeBoard(0x00030000) shouldBe 0x300000
+        transposeBoard(0x0000C000) shouldBe 0xc000
     }
     it should "transpose to hex string" in {
-        transpose(0xC0000000).toHexString shouldBe "c0000000"
-        transpose(0x30000000).toHexString shouldBe "3000000"
-        transpose(0x0C000000).toHexString shouldBe "c0000"
-        transpose(0x03000000).toHexString shouldBe "30000000"
-        transpose(0x00C00000).toHexString shouldBe "c00000"
-        transpose(0x00300000).toHexString shouldBe "30000"
-        transpose(0x000C0000).toHexString shouldBe "c000000"
-        transpose(0x00030000).toHexString shouldBe "300000"
-        transpose(0x0000C000).toHexString shouldBe "c000"
+        transposeBoard(0xC0000000).toHexString shouldBe "c0000000"
+        transposeBoard(0x30000000).toHexString shouldBe "3000000"
+        transposeBoard(0x0C000000).toHexString shouldBe "c0000"
+        transposeBoard(0x03000000).toHexString shouldBe "30000000"
+        transposeBoard(0x00C00000).toHexString shouldBe "c00000"
+        transposeBoard(0x00300000).toHexString shouldBe "30000"
+        transposeBoard(0x000C0000).toHexString shouldBe "c000000"
+        transposeBoard(0x00030000).toHexString shouldBe "300000"
+        transposeBoard(0x0000C000).toHexString shouldBe "c000"
     }
     it should "transpose to Int 2" in {
-        transpose(0xF0000000) shouldBe 0xC3000000
-        transpose(0x03C00000) shouldBe 0x30c00000
+        transposeBoard(0xF0000000) shouldBe 0xC3000000
+        transposeBoard(0x03C00000) shouldBe 0x30c00000
+    }
+    it should "transpose String" in {
+        TicTacToeInt.parse("X  X  X  ").map(x => x.transpose).get.toString() shouldBe "XXX\n...\n...\n"
+        transposeBoard(0x03C00000) shouldBe 0x30c00000
     }
     it should "get rows" in {
         row(0xC0000000, 0) shouldBe 0x30
@@ -57,79 +67,74 @@ class TicTacToeIntSpec extends AnyFlatSpec with should.Matchers {
         diagonal(0xC000) shouldBe 3
     }
 
-    behavior of "TicTacToeSpec"
+    behavior of "TicTacToeInt"
     it should "row" in {
-        val ttt = TicTacToe()
-        ttt.row(0).size shouldBe stride
-        ttt.row(0) shouldBe Seq(None, None, None)
-    }
-
-    it should "column" in {
-        val ttt = TicTacToe()
-        ttt.column(0).size shouldBe stride
-        ttt.column(0) shouldBe Seq(None, None, None)
+        val ty: Try[TicTacToeInt] = TicTacToeInt.parse("X   X   X")
+        ty.map(_.row(0)) should matchPattern { case Success(16) => }
+        ty.map(_.row(1)) should matchPattern { case Success(4) => }
+        ty.map(_.row(2)) should matchPattern { case Success(1) => }
     }
 
     it should "open" in {
-        val ttt = TicTacToe()
+        val ttt = TicTacToeInt()
         ttt.open.size shouldBe stride * stride
     }
 
     it should "play true, 0, 0" in {
-        val t0 = TicTacToe()
+        val t0 = TicTacToeInt()
         val t1 = t0.play(xOrO = true)(0, 0)
         t1.open.size shouldBe stride * stride - 1
-        t1.toString shouldBe "X  \n   \n   \n"
+        t1.toString shouldBe "X..\n...\n...\n"
     }
 
     it should "play false, 1, 0" in {
-        val t0 = TicTacToe()
+        val t0 = TicTacToeInt()
         val t1 = t0.play(xOrO = false)(1, 0)
         t1.open.size shouldBe stride * stride - 1
-        t1.toString shouldBe "   \n0  \n   \n"
+        t1.toString shouldBe "...\n0..\n...\n"
     }
 
     it should "playX 0, 0 and play0 1, 0" in {
-        val t0 = TicTacToe()
+        val t0 = TicTacToeInt()
         val t1 = t0.playX(0, 0)
         val t2 = t1.play0(1, 0)
         t2.open.size shouldBe stride * stride - 2
-        t2.toString shouldBe "X  \n0  \n   \n"
+        t2.toString shouldBe "X..\n0..\n...\n"
     }
 
     it should "parse" in {
-        TicTacToe.parse("         ") should matchPattern { case Success(TicTacToe.start) => }
-        val ty1 = TicTacToe.parse("X        ")
+        TicTacToeInt.parse("         ") should matchPattern { case Success(TicTacToeInt.start) => }
+        val ty1 = TicTacToeInt.parse("X        ")
         ty1 should matchPattern { case Success(_) => }
-        ty1.get shouldBe TicTacToe().playX(0, 0)
-        val ty2 = TicTacToe.parse("X  0     ")
+        ty1.get shouldBe TicTacToeInt().playX(0, 0)
+        val ty2 = TicTacToeInt.parse("X  0     ")
         ty2 should matchPattern { case Success(_) => }
-        ty2.get shouldBe TicTacToe().playX(0, 0).play0(1, 0)
+        ty2.get shouldBe TicTacToeInt().playX(0, 0).play0(1, 0)
     }
 
     it should "not parse" in {
-        TicTacToe.parse("") should matchPattern { case Failure(_) => }
-        TicTacToe.parse("        ") should matchPattern { case Failure(_) => }
-        TicTacToe.parse("          ") should matchPattern { case Failure(_) => }
+        TicTacToeInt.parse("") should matchPattern { case Failure(_) => }
+        TicTacToeInt.parse("        ") should matchPattern { case Failure(_) => }
+        TicTacToeInt.parse("          ") should matchPattern { case Failure(_) => }
     }
 
     it should "line row" in {
-        TicTacToe.parse("XXX      ").get.line shouldBe true
-        TicTacToe.parse("   XXX   ").get.line shouldBe true
-        TicTacToe.parse("      XXX").get.line shouldBe true
-        TicTacToe.parse("0     XXX").get.line shouldBe true
-        TicTacToe.parse("000   X  ").get.line shouldBe true
+        TicTacToeInt.parse("XXX      ").get.line shouldBe Some(true)
+        TicTacToeInt.parse("   XXX   ").get.line shouldBe Some(true)
+        TicTacToeInt.parse("      XXX").get.line shouldBe Some(true)
+        TicTacToeInt.parse("0     XXX").get.line shouldBe Some(true)
+        TicTacToeInt.parse("000   X  ").get.line shouldBe Some(false)
     }
 
     it should "line col" in {
-        TicTacToe.parse("X  X  X  ").get.line shouldBe true
-        TicTacToe.parse(" X 0X  X ").get.line shouldBe true
-        TicTacToe.parse("0 X  X  X").get.line shouldBe true
+        TicTacToeInt.parse("X  X  X  ").get.line shouldBe Some(true)
+        TicTacToeInt.parse(" X 0X  X ").get.line shouldBe Some(true)
+        TicTacToeInt.parse("0 X  X  X").get.line shouldBe Some(true)
     }
 
     it should "line diag" in {
-        TicTacToe.parse("XXX     0").get.line shouldBe true
-        TicTacToe.parse("   XXX0  ").get.line shouldBe true
-        TicTacToe.parse(" 0    XXX").get.line shouldBe true
+        TicTacToeInt.parse("XXX     0").get.line shouldBe Some(true)
+        TicTacToeInt.parse("   XXX0  ").get.line shouldBe Some(true)
+        TicTacToeInt.parse(" 0    XXX").get.line shouldBe Some(true)
     }
 }
